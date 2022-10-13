79471d5af28f2f1bf9d4761d5e32f66a82ee9071
kenny amper <kennyampss@gmail.com>
2022-10-13 15:09:10 +0800

    refactor participant

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,4 +1,61 @@
+
+import user1 from "@/assets/goTeam/image_1.png";
+import user2 from "@/assets/goTeam/image_2.png";
+import user3 from "@/assets/goTeam/image_3.png";
+import user4 from "@/assets/goTeam/image_4.png";
+import user5 from "@/assets/goTeam/image_5.png";
+import user6 from "@/assets/goTeam/image_6.png";
+import user7 from "@/assets/goTeam/image_7.png";
+
 export default {
+  user() {
+    return {
+      participantList: [
+        {
+          name: "Reyven Biloy",
+          statusIcon: "mdi-microphone-off",
+          avatar: user1,
+          status: "red",
+        },
+        {
+          name: "Bill Murray",
+          statusIcon: "mdi-microphone-outline",
+          avatar: user2,
+          status: "green",
+        },
+        {
+          name: "Helene Oliver",
+          statusIcon: "mdi-microphone-outline",
+          avatar: user3,
+          status: "green",
+        },
+        {
+          name: "Joe Carlson",
+          statusIcon: "mdi-microphone-outline",
+          avatar: user4,
+          status: "green",
+        },
+        {
+          name: "Anita Yang",
+          statusIcon: "mdi-microphone-off",
+          avatar: user5,
+          status: "red",
+        },
+        {
+          name: "Alexander Pitt",
+          statusIcon: "mdi-microphone-outline",
+          avatar: user6,
+          status: "green",
+        },
+        {
+          name: "John Doe",
+          statusIcon: "mdi-microphone-outline",
+          avatar: user7,
+          status: "green",
+        },
+      ],
+    }
+  },
 
   menu() {
     return {
